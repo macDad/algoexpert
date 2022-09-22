bcf1f10985dc7249ca9731fca7efaad9c8704db7
Maduka Jayawardana <maduka.ratnayakamudiyanselage@singtel.com>
2022-09-22 14:09:45 +0800

    Sorted Squared Array

--- src/SortedSquaredArray.java
+++ src/SortedSquaredArray.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by : <B>MadukaJ</B>
@@ -10,16 +13,30 @@ import java.util.Arrays;
  */
 public class SortedSquaredArray {
   public static void main(String[] args) {
-    int[] array = {1, 2, 3, 5, 6, 8, 9};
+    int[] array = {-2,-1,0,1, 2, 3, 5, 6, 8, 9};
     int[] array2 = {-2,-1};
+    int[] array3 = {-5,-4,-3,-2,-1};
     System.out.println(Arrays.toString(sortedSquaredArray(array)));
     System.out.println(Arrays.toString(sortedSquaredArray(array2)));
+    System.out.println(Arrays.toString(sortedSquaredArray(array3)));
   }
 
   public static int[] sortedSquaredArray(int[] array) {
+   int counter = array.length - 1;
+   int firstIndex = 0;
+   int lastIndex = array.length - 1;
+   int[] result = new int[array.length];
+
     for (int i = 0; i < array.length; i++) {
-      array[i] = array[i] * array[i];
+      if((Math.abs(array[firstIndex])) > Math.abs(array[lastIndex])){
+        result[counter - i] = array[firstIndex] * array[firstIndex];
+        firstIndex++;
+      }else {
+        result[counter - i] = array[lastIndex] * array[lastIndex];
+        lastIndex--;
+      }
     }
-    return array;
+
+   return result;
   }
 }
